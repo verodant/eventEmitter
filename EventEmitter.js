adb6c2e12ec683d1d88419d1ecfa26e15a64c979
Angel <aalcalde@elconfidencial.com>
2019-03-18 16:10:36 +0100

    añadido read.me

--- EventEmitter.js
+++ EventEmitter.js
@@ -16,31 +16,3 @@ export class EventEmitter {
         new Subject(eventName).status = context;
     }
 }
-
-
-
-
-
-
-/***/
-/* const emitter = new EventEmitter;
-emitter.on('login', (status) => {
-    console.log('holiiiii', status)
-});
-
-emitter.emit('login', true);
-emitter.emit('login', false);
-emitter.on('login', (status) => {
-    console.log('holiiiii2', status)
-});
-emitter.emit('login', false);
-
-emitter.emit('login', 'teo es gay!');
-
-emitter.on('login menuShowed', (status,remove) => {
-    console.log('estamos en uno multiple', status);
-
-    remove();
-})
- */
-/** kk */
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # eventEmitter
+
+Prueba de funcionamiento:
+
+const emitter = new EventEmitter;
+emitter.on('login', (status) => {
+    console.log('holiiiii', status)
+});
+
+emitter.emit('login', true);
+emitter.emit('login', false);
+emitter.on('login', (status) => {
+    console.log('holiiiii2', status)
+});
+emitter.emit('login', false);
+
+emitter.emit('login', 'teo es gay!');
+
+emitter.on('login menuShowed', (status,remove) => {
+    console.log('estamos en uno multiple', status);
+
+    remove();
+})
+ 
